59cbf3b866f23fef03791210620ebf0960d31ce6
rifkihariyanto25 <hariyantorifki25@gmail.com>
2025-01-27 13:31:29 +0700

    all project

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Virtual Tour School", "Virtual Tour School", "SMK Raden Umar Said", "SMK Raden Umar Said"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
+
